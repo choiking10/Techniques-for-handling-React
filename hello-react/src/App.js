04a7d679f509fc1b6d4209caa88c5b1d53ce0c8d
yunho Choi <choiking10@naver.com>
2021-08-08 19:13:58 +0900

    3.4 state

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,13 +1,10 @@
 import React, {Component} from 'react';
-import Counter from './Counter';
-import MyComponent from './MyComponent';
+import Say from './Say';
 
 class App extends Component {
   render() {
     return (
-    <>
-      <MyComponent name="React" favoriteNumber={1}> 리액트 </MyComponent>;
-    </>
+      <Say />
     )
   }
 }
