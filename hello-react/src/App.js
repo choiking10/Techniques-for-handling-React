d81174a5b8e2d1ade43499497da7e710cdca9a8d
yunho choi <choiking10@naver.com>
2021-08-10 21:29:21 +0900

    Chapter 7 Lifecycle

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,9 +1,29 @@
 import React, { Component } from 'react';
-import IterationSample from './IterationSample';
+import ErrorBoundary from './ErrorBoundary';
+import LifeCycleSample from './LifeCycleSample';
+
+function getRnadomColor() {
+  return '#' + Math.floor(Math.random() * 16777215).toString(16);
+}
 
 class App extends Component {
+  state = {
+    color: '#00000',
+  };
+  handleClick = () => {
+    this.setState({
+      color: getRnadomColor(),
+    });
+  };
   render() {
-    return <IterationSample />;
+    return (
+      <div>
+        <button onClick={this.handleClick}>랜덤 색상</button>
+        <ErrorBoundary>
+          <LifeCycleSample color={this.state.color} />
+        </ErrorBoundary>
+      </div>
+    );
   }
 }
 export default App;
