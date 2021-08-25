9e8c41bb79df398504ac6afb9ba32e0a2cd22685
Yunho Choi <choiking10@naver.com>
2021-08-25 22:09:26 +0900

    Finish chapter 14 news-viewer: custom hook, useEffect, useState, react-router-dom

--- news-viewer/src/App.js
+++ news-viewer/src/App.js
@@ -1,31 +1,13 @@
-import { useState } from 'react';
 import './App.css';
-import axios from 'axios';
-
-function App() {
-  const [data, setData] = useState(null);
-  const onClick = () => {
-    axios
-      .get('https://jsonplaceholder.typicode.com/todos/1')
-      .then((response) => {
-        setData(response.data);
-      });
-  };
+import NewsPage from './pages/NewsPage';
+import { Route } from 'react-router-dom';
+const App = () => {
+  console.log('test');
   return (
-    <div>
-      <div>
-        <button onClick={onClick}>불러오기</button>
-      </div>
-      {data && (
-        <textarea
-          row={80}
-          col={80}
-          value={JSON.stringify(data, null, 2)}
-          readOnly={true}
-        />
-      )}
-    </div>
+    <>
+      <Route path="/:category?" component={NewsPage} />
+    </>
   );
-}
+};
 
 export default App;
