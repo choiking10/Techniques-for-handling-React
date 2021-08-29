f36e4c2b0ba7d4eb3ed05cb5e87247b31b6101b8
Yunho Choi <choiking10@naver.com>
2021-08-29 15:34:03 +0900

    Finish chapter 18. redux middleware

--- learn-redux-middleware/src/index.js
+++ learn-redux-middleware/src/index.js
@@ -4,15 +4,23 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
-import rootReducer from './modules';
+import rootReducer, { rootSaga } from './modules';
 import { createStore, applyMiddleware } from 'redux';
-//import { composeWithDevTools } from 'redux-devtools-extension';
+import { composeWithDevTools } from 'redux-devtools-extension';
 //import loggerMiddleware from './lib/loggerMiddleware';
 import { createLogger } from 'redux-logger';
 import ReduxThunk from 'redux-thunk';
+import createSagaMiddleware from 'redux-saga';
 
 const logger = createLogger();
-const store = createStore(rootReducer, applyMiddleware(logger, ReduxThunk));
+const sagaMiddleware = createSagaMiddleware();
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(logger, ReduxThunk, sagaMiddleware))
+);
+
+sagaMiddleware.run(rootSaga);
+
 ReactDOM.render(
   <Provider store={store}>
     <App />
--- learn-redux-middleware/src/modules/sample.js
+++ learn-redux-middleware/src/modules/sample.js
@@ -1,6 +1,8 @@
-import { handleActions } from 'redux-actions';
+import { createAction, handleActions } from 'redux-actions';
+import { takeLatest } from 'redux-saga/effects';
 import * as api from '../lib/api';
-import createRequestThunk from '../lib/createRequestThunk';
+import createRequestSaga from '../lib/createRequestSaga';
+// import createRequestThunk from '../lib/createRequestThunk';
 
 export const GET_POST = 'sample/GET_POST';
 const GET_POST_SUCCESS = 'sample/GET_POST_SUCCESS';
@@ -8,9 +10,17 @@ const GET_POST_SUCCESS = 'sample/GET_POST_SUCCESS';
 export const GET_USERS = 'sample/GET_USERS';
 const GET_USERS_SUCCESS = 'sample/GET_USERS_SUCCESS';
 
-export const getPost = createRequestThunk(GET_POST, api.getPost);
+export const getPost = createAction(GET_POST, (id) => id);
 
-export const getUsers = createRequestThunk(GET_USERS, api.getUsers);
+export const getUsers = createAction(GET_USERS, (id) => id);
+
+const getPostSaga = createRequestSaga(GET_POST, api.getPost);
+const getUsersSaga = createRequestSaga(GET_USERS, api.getUsers);
+
+export function* sampleSaga() {
+  yield takeLatest(GET_POST, getPostSaga);
+  yield takeLatest(GET_USERS, getUsersSaga);
+}
 
 const initialState = {
   post: null,
