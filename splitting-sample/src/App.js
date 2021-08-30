751333ec92249755f2956f68d2c3c8eaee13cdf7
Yunho Choi <choiking10@naver.com>
2021-08-30 21:30:59 +0900

    loadable/component

--- splitting-sample/src/App.js
+++ splitting-sample/src/App.js
@@ -1,22 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
-import React, { Suspense, useState } from 'react';
+import React, { useState } from 'react';
+import loadable from '@loadable/component';
 
-const SplitMe = React.lazy(() => import('./SplitMe'));
+const SplitMe = loadable(() => import('./SplitMe'), {
+  fallback: <div>loading...</div>,
+});
 
 function App() {
   const [visible, setVisible] = useState(false);
   const onClick = () => {
     setVisible(true);
   };
+  const onMouseOver = () => {
+    SplitMe.preload();
+  };
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p onClick={onClick}>HellogReact!</p>
-        <Suspense fallback={<div>loading...</div>}>
-          {visible && <SplitMe />}
-        </Suspense>
+        <p onClick={onClick} onMouseOver={onMouseOver}>
+          HellogReact!
+        </p>
+        {visible && <SplitMe />}
       </header>
     </div>
   );
