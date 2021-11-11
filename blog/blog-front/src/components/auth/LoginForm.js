e02fc28fbd4bd4945b02d93cbffb23886af38b63
Yunho Choi <choiking10@naver.com>
2021-11-11 19:47:33 +0900

    Chapter24.2 connect api using saga

--- blog/blog-front/src/components/auth/LoginForm.js
+++ blog/blog-front/src/components/auth/LoginForm.js
@@ -1,12 +1,18 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { changeField, initializeForm } from '../../modules/auth';
+import { withRouter } from 'react-router';
+import { changeField, initializeForm, login } from '../../modules/auth';
+import { check } from '../../modules/user';
 import AuthForm from './AuthForm';
 
-const LoginForm = () => {
+const LoginForm = ({ history }) => {
+  const [error, setError] = useState(null);
   const dispatch = useDispatch();
-  const { form } = useSelector(({ auth }) => ({
+  const { form, auth, authError, user } = useSelector(({ auth, user }) => ({
     form: auth.login,
+    auth: auth.auth,
+    authError: auth.authError,
+    user: user.user,
   }));
 
   const onChange = (e) => {
@@ -15,19 +21,41 @@ const LoginForm = () => {
   };
   const onSubmit = (e) => {
     e.preventDefault();
+    const { username, password } = form;
+
+    dispatch(login({ username, password }));
   };
 
   useEffect(() => {
     dispatch(initializeForm('login'));
   }, [dispatch]);
+
+  useEffect(() => {
+    if (authError) {
+      console.log('로그인 실패');
+      setError('로그인 실패');
+    }
+    if (auth) {
+      console.log('로그인 성공');
+      dispatch(check());
+    }
+  }, [authError, auth, dispatch, setError]);
+
+  useEffect(() => {
+    if (user) {
+      history.push('/');
+    }
+  });
+
   return (
     <AuthForm
       type="login"
       form={form}
       onChange={onChange}
       onSubmit={onSubmit}
+      error={error}
     />
   );
 };
 
-export default LoginForm;
+export default withRouter(LoginForm);
--- blog/blog-front/src/index.js
+++ blog/blog-front/src/index.js
@@ -4,13 +4,19 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
-import rootReducer from './modules';
+import rootReducer, { rootSaga } from './modules';
+import createSagaMiddleware from 'redux-saga';
 import { composeWithDevTools } from 'redux-devtools-extension';
 
-const store = createStore(rootReducer, composeWithDevTools());
+const sagaMiddleware = createSagaMiddleware();
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(sagaMiddleware))
+);
 
+sagaMiddleware.run(rootSaga);
 ReactDOM.render(
   <Provider store={store}>
     <BrowserRouter>
